8d5741a175023bb6c795f34bfc356923d303c2dc
Dnzo <dangelsfocus@gmail.com>
2023-11-16 23:52:16 -0500

    sdsa

--- src/Composables/axios.js
+++ src/Composables/axios.js
@@ -2,7 +2,7 @@
 import Axios from "axios";
 
 const axiosInstance = Axios.create({
-  baseURL: "http://192.168.10.11:8000",
+  baseURL: "http://10.24.216.215:8000",
   // baseURL: "http://127.0.0.1:8000", // Cambia esto a tu URL base
 });
 
